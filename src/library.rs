956be574ac31f5a16624a6064884890768ed2ce1
ArcticPheenix <phoenix.ckelly@gmail.com>
2022-01-14 08:06:38 -0700

    Added unit tests for library module

--- src/library.rs
+++ src/library.rs
@@ -67,3 +67,55 @@ impl Deref for Library {
         &self.books
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_library() {
+        // Initialize library test
+        let mut lib = Library::new();
+        let book1 = Book {
+            author: "Mark Twain".to_string(),
+            title: "Huckleberry Finn".to_string(),
+            year: 1876,
+            isbn: "012-34567-890".to_string()
+        };
+        let book2 = Book {
+            author: "Mark Twain".to_string(),
+            title: "Tom Sawyer".to_string(),
+            year: 1877,
+            isbn: "123-45678-901".to_string()
+        };
+        let book3 = Book {
+            author: "Dingus McDingleberry".to_string(),
+            title: "Being a Dingleberry".to_string(),
+            year: 2021,
+            isbn: "321-65432-901".to_string()
+        };
+        lib.add_book(book1);
+        lib.add_book(book2);
+        lib.add_book(book3);
+
+        // Test get_book
+        match lib.get_book("012-34567-890") {
+            Some(book) => {
+                assert_eq!(book.title, "Huckleberry Finn".to_string())
+            },
+            None => panic!("Didn't retrieve the right book!")
+        };
+
+        // Test get_books
+        let result = lib.get_books();
+        assert_eq!(result.len(), 3, "Incorrect number of books.");
+
+        // Test search_book
+        let result = lib.search_book("Mark Twain");
+        assert_eq!(result.len(), 2, "Incorrect number of books.");
+
+        // Test remove_book
+        let result = lib.remove_book("012-34567-890");
+        assert_eq!(result.unwrap(), "Removed".to_string());
+    }
+}
